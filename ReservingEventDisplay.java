756a90d172524cd91656b33fd706aa47bb693a07
mariamanauis <90587965+mariamanauis@users.noreply.github.com>
2023-02-26 00:11:19 +0800

    Add files via upload

--- ReservingEventDisplay.java
+++ ReservingEventDisplay.java
@@ -11,39 +11,73 @@ import java.awt.*;
  * @author HP
  */
 public class ReservingEventDisplay extends JFrame {
-    private JLabel budget, eventImg, name, location, entrance, availability, cost, question;
+    private JLabel background, budget, eventImg, name, location, entrance, availability, cost, question;
     private JButton yes, no;
-    private JPanel southPanel, eastPanel;
+    private JPanel southPanel, centerPanel;
     
     public ReservingEventDisplay(Event e){
         super("Get That Groove Back!");
         this.setLayout(new BorderLayout(10, 10));
         this.budget = new JLabel("Budget: ");
-        this.eventImg = new JLabel();
+        
+        centerPanel = new JPanel();
+        centerPanel.setLayout(new GridBagLayout());
+        GridBagConstraints con = new GridBagConstraints();
+        con.fill = GridBagConstraints.BOTH;
+       
+        
+        eventImg = new JLabel();
         ImageIcon event = new ImageIcon(ReservingEventDisplay.class.getResource(e.getImgFileName()));
-        eventImg.setIcon(event);
-        this.name = new JLabel(e.getName());
-        this.location = new JLabel(e.getLocation());
-        this.entrance = new JLabel(String.valueOf(e.getEntranceFee()));
-        this.availability = new JLabel(String.valueOf(e.getAvailability()));
-        this.cost = new JLabel(String.valueOf(e.getCost()));
-        this.eastPanel = new JPanel();
-        eastPanel.setLayout(new BoxLayout(eastPanel, BoxLayout.Y_AXIS));
+        this.eventImg.setIcon(event);
+        con.gridwidth = 1;
+        con.gridheight = 5;
+        con.gridx = 0;
+        con.gridy = 0;
+        centerPanel.add(eventImg, con);
         
-        eastPanel.add(name);
-        eastPanel.add(location);
-        eastPanel.add(entrance);
-        eastPanel.add(cost);
-        eastPanel.add(availability);
+        name = new JLabel(e.getName());
+        con.gridwidth = 1;
+        con.gridheight = 1;
+        con.gridx = 1;
+        con.gridy = 0;
+        centerPanel.add(name, con);
         
-        this.southPanel = new JPanel();
+        location = new JLabel("Location: " + e.getLocation());
+        con.gridwidth = 1;
+        con.gridheight = 1;
+        con.gridx = 1;
+        con.gridy = 1;
+        centerPanel.add(location, con);
+        
+        entrance = new JLabel("Entrance Fee: " + String.valueOf(e.getEntranceFee())); 
+        con.gridwidth = 1;
+        con.gridheight = 1;
+        con.gridx = 1;
+        con.gridy = 2;
+        centerPanel.add(entrance, con);
+        
+        cost = new JLabel("Cost: " + String.valueOf(e.getCost()));
+        con.gridwidth = 1;
+        con.gridheight = 1;
+        con.gridx = 1;
+        con.gridy = 3;
+        centerPanel.add(cost, con);
+        
+        availability = new JLabel("Availability: " + String.valueOf(e.getAvailability()));
+        con.gridwidth = 1;
+        con.gridheight = 1;
+        con.gridx = 1;
+        con.gridy = 4;
+        centerPanel.add(availability, con);
+        
+        southPanel = new JPanel();
         
     
         southPanel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.HORIZONTAL;
         
-        this.question = new JLabel("Reserve this event?");
+        question = new JLabel("Reserve this event?");
         c.gridwidth = 2;
         c.gridx = 0;
         c.gridy = 0;
@@ -51,21 +85,20 @@ public class ReservingEventDisplay extends JFrame {
         
         southPanel.add(question, c);
         
-        this.yes = new JButton("Yes");
+        yes = new JButton("Yes");
         c.gridwidth = 1;
         c.gridx = 0;
         c.gridy = 1;
         southPanel.add(yes, c);
         
-        this.no = new JButton("No");
+        no = new JButton("No");
         c.gridwidth = 1;
         c.gridx = 1;
         c.gridy = 1;
         southPanel.add(no, c);
         
         this.add(budget, BorderLayout.NORTH);
-        this.add(eventImg, BorderLayout.CENTER);
-        this.add(eastPanel, BorderLayout.EAST);
+        this.add(centerPanel, BorderLayout.CENTER);
         this.add(southPanel, BorderLayout.SOUTH);
         
         this.setSize(900, 600);
--- main.java
+++ main.java
@@ -8,7 +8,8 @@ package getthatgrooveback;
  *
  * @author HP
  */
-public class main {
+public class main{
+    
     public static void main(String[] args){
         /*Member[] party = new Member[6];
         party[0] = new Member("Jark Pihyo", "biology.png", "dancing", 0, 0);
@@ -23,9 +24,10 @@ public class main {
         Event event1 = new Event("MKNS Stage", "biology.png", 200, "location", 1000, true);
         ReservingEventDisplay frame2 = new ReservingEventDisplay(event1);
         
+        
         //frame1.setVisible(true);
         frame2.setVisible(true);
-        
+
         
     }
 }
